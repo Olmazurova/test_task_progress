15c54178aded730c5a05fad2087a959af3ba4528
Olga Mazurova <vsilena@mail.ru>
2024-10-12 19:50:28 +0300

    Добавлены страницыÐ, коаÐ, изменён шаблонÐ, добавлены стили

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
-@app.route("/index")
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
+
 @app.route("/")
+@app.route("/index")
 def index():
+    print(url_for('index')) # аргументом указываем имя функции index
     return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username, path):
+    return f"Пользователь: {username}"
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
+# with app.test_request_context():  # искусственное создание запроса, чтобы посмотреть как работает функция
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username='selfedu'))
 
 if __name__ == "__main__":
     app.run(debug=True)
