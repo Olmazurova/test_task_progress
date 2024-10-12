dfc8e432f45f3255964cef95a2647644e2c96212
Olga Mazurova <vsilena@mail.ru>
2024-10-12 21:25:26 +0300

    ÐИÑÑзменены названия страниÑцÐ, поправлены шаблоны страниÑц

--- app.py
+++ app.py
@@ -2,21 +2,20 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = [{"name": "Установка", "url": "install-flask"},
-        {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "contact"}]
+menu = [{"name": "Информация", "url": "information"},
+        {"name": "Достижения", "url": "achievements"},
+        {"name": "Статистика", "url": "statistics"}]
 
 @app.route("/")
-@app.route("/index")
 def index():
     print(url_for('index')) # аргументом указываем имя функции index
     return render_template('index.html', menu=menu)
 
 
-@app.route("/about")
-def about():
-    print(url_for('about'))
-    return render_template('about.html', title="О сайте", menu=menu)
+@app.route("/achievements")
+def achievements():
+    print(url_for('achievements'))
+    return render_template('achievements.html', title="Достижения", menu=menu)
 
 
 @app.route("/profile/<username>")
@@ -24,11 +23,11 @@ def profile(username, path):
     return f"Пользователь: {username}"
 
 
-@app.route("/contact", methods=["POST", "GET"])
-def contact():
+@app.route("/statistics", methods=["POST", "GET"])
+def statistics():
     if request.method == 'POST':
         print(request.form)
-    return render_template('contact.html', title="Обратная связь", menu=menu)
+    return render_template('statistics.html', title="Статистика достижений", menu=menu)
 
 # with app.test_request_context():  # искусственное создание запроса, чтобы посмотреть как работает функция
 #     print(url_for('index'))
