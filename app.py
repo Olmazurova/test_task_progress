8f498a2ae3685ffc492c958d1bb0f03eb0e051bd
Olga Mazurova <vsilena@mail.ru>
2024-10-19 23:56:38 +0300

    Добавлена авторизация и перенаправление ссылокÐ

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456789qwerty'
 menu = [{"name": "Информация", "url": "information"},
         {"name": "Достижения", "url": "achievements"},
         {"name": "Статистика", "url": "statistics"}]
+
 
 @app.route("/")
 def index():
@@ -24,11 +25,6 @@ def achievements():
     return render_template('achievements.html', title="Достижения", menu=menu)
 
 
-@app.route("/profile/<username>")
-def profile(username, path):
-    return f"Пользователь: {username}"
-
-
 @app.route("/statistics")
 def statistics():
     return render_template('statistics.html', title="Статистика достижений", menu=menu)
@@ -37,6 +33,30 @@ def statistics():
 #     print(url_for('index'))
 #     print(url_for('about'))
 #     print(url_for('profile', username='selfedu'))
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Профиль пользователя: {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
